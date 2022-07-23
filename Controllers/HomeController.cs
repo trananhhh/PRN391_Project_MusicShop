c8e090734995ef47f871af74d88c9c55e487b677
Nguyen Tran Quoc Anh <32959690+trananhhh@users.noreply.github.com>
2022-07-24 01:46:40 +0700

    Login/Signup UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -96,5 +96,9 @@ namespace Project_MusicShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
